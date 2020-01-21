99450ef0df9f6400d1056a862fa5d92b2ebbc126
sally <you@example.com>
2020-01-21 15:21:37 +0000

    add schema, data source and resolvers

---
+++ start/server/src/index.js
@@ -0,0 +1,51 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');//this is the schema
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+const isEmail = require('isemail');
+
+const store = createStore();
+
+const server = new ApolloServer({ 
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        const email = Buffer.from(auth, 'base64').toString('ascii');
+        if (!isEmail.validate(email)) return { user: null };
+        // find a user by their email
+        const users = await store.users.findOrCreate({ where: { email } });
+        const user = users && users[0] || null;
+    
+        return { user: { ...user.dataValues } };
+      },
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
+server.listen().then(({ url }) => {
+    console.log( `Server ready at ${url}`);
+    const { ApolloServer } = require('apollo-server');
+    const typeDefs = require('./schema');
+    const { createStore } = require('./utils');
+
+    const LaunchAPI = require('./datasources/launch');
+    const UserAPI = require('./datasources/user');
+
+    const store = createStore();
+
+    const server = new ApolloServer({
+    typeDefs,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(), //add this to context so that they can be called 4 use
+        userAPI: new UserAPI({ store })
+    })
+    });
+
+    
+});
